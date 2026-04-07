2769e39ea5b7adc78b85e635d3fa33795a0005bc
Surya <saudsurya10@gmail.com>
2026-04-08 01:31:16 +0545

    create jwt login inside app accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RegisterView
+from .views import ProfileView, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', ProfileView.as_view(), name='profile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,21 @@
 from rest_framework import generics
 from .serializers import RegisterSerializer
 from .models import User
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer  
+
+
+class ProfileView(APIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'email': request.user.email,
+            'name': request.user.name
+        })
